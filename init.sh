3fd4b58f876b53238032483f8681c0a328adb13e
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-01-09 18:30:32 +0530

    Keep track of platform specific scripts

---
+++ init.sh
@@ -0,0 +1,12 @@
+cp focal-server-cloudimg-amd64.img sda.img
+qemu-img resize sda.img +10G
+qemu-system-x86_64 \
+-accel kvm \
+-cpu host \
+-m 1G \
+-smp 1 \
+-device e1000,netdev=net0 \
+-netdev user,id=net0,hostfwd=tcp::2222-:22 \
+-drive file=sda.img \
+-nographic \
+-smbios type=1,serial="ds=nocloud-net;s=http://10.0.2.2:8000/"
