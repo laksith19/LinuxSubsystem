9d42738c3a94fcb6404d02180b93ce714d4b09e1
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-01-10 16:34:56 +0530

    made init display nothing

--- linux-scripts/init.sh
+++ linux-scripts/init.sh
@@ -8,5 +8,6 @@ qemu-system-x86_64 \
 -device e1000,netdev=net0 \
 -netdev user,id=net0,hostfwd=tcp::2222-:22 \
 -drive file=sda.img \
--nographic \
+-display none \
+-nodefaults \
 -smbios type=1,serial="ds=nocloud-net;s=http://10.0.2.2:8000/"
