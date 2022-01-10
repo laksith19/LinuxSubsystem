5a46bf39892d4881e0b2b6bf11314dce75fc7a04
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-01-11 00:35:50 +0530

    Added code to check if port is free

--- LinuxSubsystem.py
+++ LinuxSubsystem.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-import sys, platform, subprocess
+import sys, platform, subprocess, socket
 
 
 if __name__ == '__main__':
-	print("Nothing has been implemented")	
+    print("Nothing has been implemented")	
+
+
+def is_port_in_use(port):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        return s.connect_ex(('localhost', port)) == 0
