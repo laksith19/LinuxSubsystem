9b419849cc7afdf0711231a9f25c21385fa1750d
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-01-15 22:28:37 +0530

    remove main method

--- LinuxSubsystem/LinuxSubsystem.py
+++ LinuxSubsystem/LinuxSubsystem.py
@@ -3,10 +3,6 @@
 import sys, platform, subprocess, socket
 
 
-if __name__ == '__main__':
-    print("Nothing has been implemented")	
-
-
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('localhost', port)) == 0
