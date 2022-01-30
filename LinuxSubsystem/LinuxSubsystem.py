20089ef10f6677033f29c275f040011fe105531d
laksith19 <laksith@berkeley.edu>
2022-01-29 18:54:09 -0800

    use loop instead of recursion

--- LinuxSubsystem/LinuxSubsystem.py
+++ LinuxSubsystem/LinuxSubsystem.py
@@ -7,11 +7,15 @@ def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(('localhost', port)) == 0
 
+def get_username():
+   pass 
+
 def get_password():
     print("User " + getpass.getuser() + " will be created")
-    p = getpass.getpass(prompt='New Passord:')
-    p2 = getpass.getpass(prompt='Confirm Password:')
-    if p != p2:
-        print("Passwords don't match, resetting")
-        return get_password()
+    while True:
+        p = getpass.getpass(prompt='New Passord:')
+        p2 = getpass.getpass(prompt='Confirm Password:')
+        if p == p2:
+            break
+        print("Passwords don't match! Resetting, re-enter passwords.")
     return crypt.crypt(p, crypt.mksalt(crypt.METHOD_SHA512))
