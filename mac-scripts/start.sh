cf67a9497832c2363704c4910a56d1838b677608
laksith <laksith@sly.lmi.net>
2022-02-09 19:24:12 -0800

    Added mac-specific scripts

---
+++ mac-scripts/start.sh
@@ -0,0 +1,10 @@
+qemu-system-x86_64 \
+-accel hvf \
+-cpu max \
+-m 1G \
+-smp 1 \
+-device e1000,netdev=net0 \
+-netdev user,id=net0,hostfwd=tcp::2222-:22 \
+-drive file=sda.img \
+-display none \
+-nodefaults \
