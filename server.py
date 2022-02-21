abb8ae174aa8c64d008e8dd12fff699bec433f84
laksith19 <laksith@berkeley.edu>
2022-02-21 03:12:20 -0800

    Use os again to specify a proper path

--- server.py
+++ server.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env  python3
 
 import http.server
-import socketserver
 import os
 
 PORT = 8000
+FLAG = True
 
-class noCloudServer(http.server.SimpleHTTPRequestHandler):
+class noCloudServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path != '/user-data' and  self.path != '/meta-data':
-            self.send_responsde(404);
+            FLAG = False
+            self.send_error(404);
             return
-        self.path = 'www' + self.path
-        return http.server.SimpleHTTPRequestHandler.do_GET(self)
-
+        self.send_response(200)
+        self.send_header("Content-type", "application/octet-stream");
+        self.end_headers()
+        content = open(os.path.join(os.path.realpath('www'), self.path[1:]), 'rb').read()
+        self.wfile.write(content) 
 Handler = noCloudServer
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
+with http.server.HTTPServer(("", PORT), Handler) as httpd:
     print("Http Server Serving at port", PORT)
-    httpd.serve_forever()
+    while FLAG:
+        httpd.handle_request()
+    httpd.server_close()
+
+print("test, got here")
