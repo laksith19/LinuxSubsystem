3822639babbce481b7f765b90c8cbfbfaa0eb702
laksith19 <laksith@berkeley.edu>
2022-02-22 03:13:34 -0800

    Made serever a bit cleaner

--- server.py
+++ server.py
@@ -3,10 +3,13 @@
 import http.server
 import os
 
+# The port where the config is served
 PORT = 8000
+
+# The data to be served in the form of a python dict
 CONTENT = { 
-    'meta-data' : b'{\n"instance-id": "iid-local01"\n}\n', 
-    'user-data' : b'#cloud-config\nusers:\n    - name: laksith\n      shell: /bin/bash\n      passwd: $6$8pLLJrEQ21ZZmaxD$xmNWRDukc/aj3O37ZGWR/1pC1jkJ7bbMIteiXdExGlm8tUdlR/Hel3DOzx7Cto2Fcn3XIWBevZHky6HNafP710 \n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
+    '/meta-data' : b'{\n"instance-id": "iid-local01"\n}\n', 
+    '/user-data' : b'#cloud-config\nusers:\n    - name: laksith\n      shell: /bin/bash\n      passwd: $6$8pLLJrEQ21ZZmaxD$xmNWRDukc/aj3O37ZGWR/1pC1jkJ7bbMIteiXdExGlm8tUdlR/Hel3DOzx7Cto2Fcn3XIWBevZHky6HNafP710 \n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
 }
 
 class noCloudServer(http.server.BaseHTTPRequestHandler):
@@ -18,7 +21,7 @@ class noCloudServer(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/octet-stream");
         self.end_headers()
-        self.wfile.write(CONTENT[self.path[1:]]) 
+        self.wfile.write(CONTENT[self.path]) 
 Handler = noCloudServer
 with http.server.HTTPServer(("", PORT), Handler) as httpd:
     print("Http Server Serving at port", PORT)
