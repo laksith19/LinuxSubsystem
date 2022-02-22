cc6e4e7c4f4e65afc4b035698289c92c96ed2ed6
laksith19 <laksith@berkeley.edu>
2022-02-21 23:23:46 -0800

    Serve config without writing to file

--- server.py
+++ server.py
@@ -4,7 +4,10 @@ import http.server
 import os
 
 PORT = 8000
-FLAG = True
+CONTENT = { 
+    'meta-data' : b'{\n"instance-id": "iid-local01"\n}\n', 
+    'user-data' : b'#cloud-config\nusers:\n    - name: laksith\n      shell: /bin/bash\n      passwd: $6$8pLLJrEQ21ZZmaxD$xmNWRDukc/aj3O37ZGWR/1pC1jkJ7bbMIteiXdExGlm8tUdlR/Hel3DOzx7Cto2Fcn3XIWBevZHky6HNafP710 \n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
+}
 
 class noCloudServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,13 +18,12 @@ class noCloudServer(http.server.BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "application/octet-stream");
         self.end_headers()
-        content = open(os.path.join(os.path.realpath('www'), self.path[1:]), 'rb').read()
-        self.wfile.write(content) 
+        self.wfile.write(CONTENT[self.path[1:]]) 
 Handler = noCloudServer
 with http.server.HTTPServer(("", PORT), Handler) as httpd:
     print("Http Server Serving at port", PORT)
     i = 0
-    while FLAG and i < 10:
+    while i < 10:
         httpd.handle_request()
         i += 1
     httpd.server_close()
