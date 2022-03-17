75b2950a288aebc4344a8990a1023e4e58dd5970
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-03-17 13:12:10 -0700

    Added crypt library back and tried to use f strings to serve cloud init data

--- LinuxSubsystem.py
+++ LinuxSubsystem.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import os
-#import crypt
+import crypt
 import getpass
 import socket
 
--- server.py
+++ server.py
@@ -5,23 +5,24 @@ import os
 
 # The port where the config is served
 PORT = 8000
+USER = 'laksith'
+PASSWD = '$6$8pLLJrEQ21ZZmaxD$xmNWRDukc/aj3O37ZGWR/1pC1jkJ7bbMIteiXdExGlm8tUdlR/Hel3DOzx7Cto2Fcn3XIWBevZHky6HNafP710'
 
 # The data to be served in the form of a python dict
 CONTENT = { 
-    '/meta-data' : b'{\n"instance-id": "iid-local01"\n}\n', 
-    '/user-data' : b'#cloud-config\nusers:\n    - name: laksith\n      shell: /bin/bash\n      passwd: $6$8pLLJrEQ21ZZmaxD$xmNWRDukc/aj3O37ZGWR/1pC1jkJ7bbMIteiXdExGlm8tUdlR/Hel3DOzx7Cto2Fcn3XIWBevZHky6HNafP710 \n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
+    '/meta-data' : '{\n"instance-id": "iid-local01"\n}\n', 
+    '/user-data' : f'#cloud-config\nusers:\n    - name: {USER}\n      shell: /bin/bash\n      passwd: {PASSWD}\n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
 }
 
 class noCloudServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path != '/user-data' and  self.path != '/meta-data':
-            FLAG = False
             self.send_error(404);
             return
         self.send_response(200)
         self.send_header("Content-type", "application/octet-stream");
         self.end_headers()
-        self.wfile.write(CONTENT[self.path]) 
+        self.wfile.write(CONTENT[self.path].encode('utf-8')) 
 Handler = noCloudServer
 with http.server.HTTPServer(("", PORT), Handler) as httpd:
     print("Http Server Serving at port", PORT)
