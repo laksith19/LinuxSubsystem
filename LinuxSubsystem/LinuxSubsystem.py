91d19f5c32761d59370174c6f875796c2adc6a36
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-03-17 14:01:04 -0700

    Moved Content generation from server to subsystem

--- LinuxSubsystem/LinuxSubsystem.py
+++ LinuxSubsystem/LinuxSubsystem.py
@@ -18,7 +18,7 @@ def get_username():
 def get_password():
     # Tries to get a valid password match from teh user before qutting with an error 
     for _ in range(3):
-        passwd = getpass.getpass(prompt='New Passord:')
+        passwd = getpass.getpass(prompt='Password:')
         passwd_2 = getpass.getpass(prompt='Confirm Password:')
         if passwd == passwd_2:
             return crypt.crypt(passwd, crypt.mksalt(crypt.METHOD_SHA512))
@@ -27,7 +27,10 @@ def get_password():
     os._exit(255)
 
 def generate_config():
-    name = get_username()
-    password = get_password()
-    config = { meta-data : ,user-data : f'#cloud-config\nusers:\n    - name: {name}\n      shell: /bin/bash\n      passwd: {password}\n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
+    USER = get_username()
+    PASSWD  = get_password()
+    config = { 
+            '/meta-data' : '{\n"instance-id": "iid-local01"\n}\n',
+            '/user-data' : f'#cloud-config\nusers:\n    - name: {USER}\n      shell: /bin/bash\n      passwd: {PASSWD}\n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
+}
     return config
--- LinuxSubsystem/server.py
+++ LinuxSubsystem/server.py
@@ -5,14 +5,6 @@ import os
 
 # The port where the config is served
 PORT = 8000
-USER = 'laksith'
-PASSWD = '$6$8pLLJrEQ21ZZmaxD$xmNWRDukc/aj3O37ZGWR/1pC1jkJ7bbMIteiXdExGlm8tUdlR/Hel3DOzx7Cto2Fcn3XIWBevZHky6HNafP710'
-
-# The data to be served in the form of a python dict
-CONTENT = { 
-    '/meta-data' : '{\n"instance-id": "iid-local01"\n}\n', 
-    '/user-data' : f'#cloud-config\nusers:\n    - name: {USER}\n      shell: /bin/bash\n      passwd: {PASSWD}\n      lock_passwd: false\n      sudo: [\'ALL=(ALL) ALL\']\nssh_pwauth: True\nhostname: linuxsubsystem\nruncmd:\n    - echo "blacklist floppy" | tee /etc/modprobe.d/blacklist-floppy.conf\n    - rmmod floppy\n    - update-initramfs -u\n    - touch /etc/cloud/cloud-init.disabled\n    - hostnamectl set-hostname linuxsubsystem\npower_state:\n    mode: poweroff\n    message: Shutting Down\n    timeout: 5\n    condition: True\n'
-}
 
 class noCloudServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
