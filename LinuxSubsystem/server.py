b8b60dffe1fc92f3308f55b12cde135f38673d9f
Laksith Venkatesh Prabu <laksith@berkeley.edu>
2022-03-17 15:21:38 -0700

    Break when requesting vendor data

--- LinuxSubsystem/server.py
+++ LinuxSubsystem/server.py
@@ -4,23 +4,27 @@ import LinuxSubsystem
 import http.server
 import os
 
-# The port where the config is served
+FLAG = []
 PORT = 8000
 CONTENT = LinuxSubsystem.generate_config()
 class noCloudServer(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         if self.path != '/user-data' and  self.path != '/meta-data':
+            if self.path == '/vendor-data':
+                FLAG.append(1)
             self.send_error(404);
             return
         self.send_response(200)
         self.send_header("Content-type", "application/octet-stream");
         self.end_headers()
         self.wfile.write(CONTENT[self.path].encode('utf-8')) 
-Handler = noCloudServer
-with http.server.HTTPServer(("", PORT), Handler) as httpd:
+
+with http.server.HTTPServer(("", PORT), noCloudServer) as httpd:
     print("Http Server Serving at port", PORT)
     i = 0
-    while i < 10:
+    while True:
         httpd.handle_request()
+        if FLAG:
+            break
         i += 1
     httpd.server_close()
