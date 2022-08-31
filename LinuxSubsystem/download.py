84c073973ab8ed0e3b2b78edbc221dbb93f01f95
laksith19 <laksith@berkeley.edu>
2022-08-31 15:04:55 -0700

    Clean up download and restructure

--- LinuxSubsystem/download.py
+++ LinuxSubsystem/download.py
@@ -5,20 +5,21 @@ import shutil
 URL = 'https://cloud-images.ubuntu.com/'\
         'jammy/current/jammy-server-cloudimg-amd64.img'
 FILE = 'base.img'
-request = urllib.request.Request(URL)
+REQUEST= urllib.request.Request(URL)
+
+
 print("Starting Download of base image")
-with urllib.request.urlopen(request) as response:
-    total_size = int(response.info()['Content-Length'])
+with urllib.request.urlopen(REQUEST) as RESPONSE:
+    total_size = int(RESPONSE.info()['Content-Length'])
     completion = 0
     with open(FILE, 'wb') as f:
         while True:
-            buffer = response.read(shutil.COPY_BUFSIZE)
+            buffer = RESPONSE.read(shutil.COPY_BUFSIZE)
             if not buffer:
                 break
             f.write(buffer)
             completion += len(buffer) / total_size
-            print("Downloading: ", completion * 100, "%")
+            print("Downloading: ", f'{completion * 100:.2f}', "%", end='\r')
 
-
-print("Download Completed!")
+print("\nDownload Completed!")
 
