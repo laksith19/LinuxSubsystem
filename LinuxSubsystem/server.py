f5ede104c91dd384b2792d5d4de253f758744dc7
laksith19 <laksith@berkeley.edu>
2022-09-05 20:48:02 -0700

    Add download logs

--- LinuxSubsystem/server.py
+++ LinuxSubsystem/server.py
@@ -1,30 +1,41 @@
 #!/usr/bin/env  python3
 
-import LinuxSubsystem
+import utils
 import http.server
 import os
 
-FLAG = []
-PORT = 8000
-CONTENT = LinuxSubsystem.generate_config()
-class noCloudServer(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        if self.path != '/user-data' and  self.path != '/meta-data':
-            if self.path == '/vendor-data':
-                FLAG.append(1)
-            self.send_error(404);
-            return
-        self.send_response(200)
-        self.send_header("Content-type", "application/octet-stream");
-        self.end_headers()
-        self.wfile.write(CONTENT[self.path].encode('utf-8')) 
+def serveConfig():
+    # This is really scuffed but the easiest way to create a persitant update
+    FLAG = []
+    PORT = 8000
+    CONTENT = utils.generate_config()
+    LOG_FILE = "http-logs"
+    class noCloudServer(http.server.BaseHTTPRequestHandler):
+        def log_message(self, format, *args):
+            with open(LOG_FILE, "a") as logger:
+                logger.write("%s - - [%s] %s\n" %
+                         (self.address_string(),
+                          self.log_date_time_string(),
+                          format%args))
+        def do_GET(self):
+            if self.path != '/user-data' and  self.path != '/meta-data':
+                if self.path == '/vendor-data':
+                    FLAG.append(1)
+                self.send_error(404);
+                return
+            self.send_response(200)
+            self.send_header("Content-type", "application/octet-stream");
+            self.end_headers()
+            self.wfile.write(CONTENT[self.path].encode('utf-8')) 
 
-with http.server.HTTPServer(("", PORT), noCloudServer) as httpd:
-    print("Http Server Serving at port", PORT)
-    i = 0
-    while True:
-        httpd.handle_request()
-        if FLAG:
-            break
-        i += 1
-    httpd.server_close()
+    with http.server.HTTPServer(("", PORT), noCloudServer) as httpd:
+        print("Serving config files...")
+        i = 0
+        while True:
+            httpd.handle_request()
+            if FLAG:
+                break
+            i += 1
+        httpd.server_close()
+        print("Completed!")
+        print("Configuring the VM. This could take a while.")
